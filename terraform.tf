14691756a8d5d6ce4c83b71463c7caa00bd8cffc
karthi-n6 <120242462+karthi-n6@users.noreply.github.com>
2025-08-25 21:52:35 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,25 @@
-variable "vm_names" {
-  description = "List of VM names"
-  type        = list(string)
-  default     = ["vm1"]
+variable "resource_group_name" {
+  description = "Name of the existing resource group"
+  type        = string
+}
+
+variable "vm_instances" {
+  description = "Map of VM configurations"
+  type = map(object({
+    name     = string
+    size     = string
+    location = string
+    os_type  = string
+  }))
 }
 
 resource "azurerm_linux_virtual_machine" "vms" {
-  for_each            = toset(var.vm_names)
-  name                = each.key
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  size                = "Standard_B1s"
-
+  for_each            = var.vm_instances
+  name                = each.value.name
+  resource_group_name = var.resource_group_name
+  location            = each.value.location
+  size                = each.value.size
+}
   admin_username      = "azureuser"
 
   network_interface_ids = [
